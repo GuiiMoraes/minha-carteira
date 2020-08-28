12c673141344bc0306b8af199a605908eba8493f
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-08-28 19:02:21 -0300

    feat: create moviment cards in dashboard

---
+++ src/components/WalletBox/styles.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+interface IContainerProps {
+  color: string;
+}
+
+export const Container = styled.div<IContainerProps>`
+  position: relative;
+
+  width: 100%;
+  height: 200px;
+
+  border-radius: 8px;
+
+  padding: 8px 16px;
+
+  color: ${props => props.theme.colors.white};
+  background-color: ${props => props.color};
+
+  overflow: hidden;
+
+  :not(:first-child) {
+    margin-left: 16px;
+  }
+
+  > img {
+    position: absolute;
+    top: -10px;
+    right: -30px;
+
+    height: 110%;
+
+    opacity: 0.3;
+  }
+
+  > strong {
+    font-size: 3.5rem;
+
+    width: 100%;
+
+    display: inline-block;
+  }
+
+  > span {
+    font-size: 18px;
+    font-weight: 500;
+  }
+
+  > small {
+    position: absolute;
+    bottom: 10px;
+    left: 20px;
+  }
+`;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  .clear-filters {
+    padding: 8px 16px;
+
+    border: none;
+    border-radius: 8px;
+
+    margin-left: 16px;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
